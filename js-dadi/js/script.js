9c8b1ef38d82d13846fc4b4e81dd80fc72baae6a
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-15 13:28:14 +0100

    gioco dei dadi fatto

---
+++ js-dadi/js/script.js
@@ -0,0 +1,13 @@
+const numeroBot = Math.floor(Math.random()*5) + 1
+console.log(numeroBot)
+
+const numeroUser = Math.floor(Math.random()*5) + 1
+console.log(numeroUser)
+
+if( numeroBot > numeroUser){
+    console.log(' Bot win, User lose')
+}else if( numeroUser > numeroBot){
+    console.log(' User win, Bot lose')
+}else if( numeroUser == numeroBot){
+    console.log('pareggio')
+}
