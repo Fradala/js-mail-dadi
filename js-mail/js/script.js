299ebf6eb2310caab7fb3c2de29489d7d10998fa
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-15 15:41:20 +0100

    mod

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,12 +1,22 @@
-const array = ['gino.22@gmail.com', 'mama.2@gmail.com', 'marta.2@gmail.com', 'mario.2@gmail.com'];
+const listaEmail = ['gino.22@gmail.com', 'mama.2@gmail.com', 'marta.2@gmail.com', 'mario.2@gmail.com'];
 
 const userEmail = document.getElementById('email').value;
 
 
 
-for (let i = 0; i <= array.length; i ++){
-    const elemento = array [i];
-    console.log('elemnto', i)
+for (let i = 0 ; i < listaEmail.length ; i ++){
+    const elemento = listaEmail[i];
+    console.log(elemento, i)
+
+   if(elemento != userEmail){
+    console.log('non ce')
+    document.getElementById ('verifica').innerHTML = ''
+
+   }else if(elemento == userEmail){
+    console.log('ce')
+    document.getElementById ('verifica').innerHTML = 'perfetto la tua email esiste'
+   }
+
 }
 
 
